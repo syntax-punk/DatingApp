2397c15550f1b23155a6e064b3a781e5b33ffd18
syntax-punk <david.jaeren@gmail.com>
2025-01-17 13:52:32 +0100

    Cleaning up webapi init structure

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,11 +14,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
