31c1c722fce0f77529835d712845fb47579ee574
syntax-punk <david.jaeren@gmail.com>
2025-01-18 23:55:24 +0100

    Added Users controller. Added https settings to the launch settings field

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        return users;
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+        if (user == null) return NotFound("We could not find the user you are looking for.");
+        
+        return user;
+    }
+}
