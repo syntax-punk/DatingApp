c2a5201c6c11e2807c09d528cf7dad52afb01be8
syntax-punk <david.jaeren@gmail.com>
2025-01-20 15:59:13 +0100

    Added cors headers. Installed ngx-bootstrap, bootstrap and font-awesome

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -22,6 +23,13 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
+
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200");
+});
 app.MapControllers();
 
 DbInitializer.InitDb(app);
