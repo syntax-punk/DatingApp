904ff10f1bc1197a930d201e5ec2b7fc3e0a4137
syntax-punk <david.jaeren@gmail.com>
2025-01-28 11:43:53 +0100

    Routing wip

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'members', component: MemberListComponent },
+  { path: 'members/:id', component: MemberDetailsComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full' },
+];
---
+++ client/src/app/members/member-details/member-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-details',
+  imports: [],
+  templateUrl: './member-details.component.html',
+  styleUrl: './member-details.component.css'
+})
+export class MemberDetailsComponent {
+
+}
