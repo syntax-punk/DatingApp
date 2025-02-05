d7aecd2b3d44bf13fbcff41474bfa13f6a7ef8a5
syntax-punk <david.jaeren@gmail.com>
2025-02-05 17:33:20 +0100

    Added error testing components

---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  imports: [],
+  templateUrl: './test-errors.component.html',
+  styleUrl: './test-errors.component.css'
+})
+export class TestErrorsComponent {
+  baseUrl = 'https://localhost:5001/api/';
+  private httpClient = inject(HttpClient);
+
+  get400Error() {
+    this.httpClient.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  get401Error() {
+    this.httpClient.get(this.baseUrl + 'buggy/auth').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  get404Error() {
+    this.httpClient.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  get500Error() {
+    this.httpClient.get(this.baseUrl + 'buggy/server-error').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  get400ValidationError() {
+    this.httpClient.post(this.baseUrl + 'account/register', {}).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+}
