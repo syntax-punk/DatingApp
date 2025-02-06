16b1e3e5ebeec7f25c3ea1ee5667818d08da64d5
syntax-punk <david.jaeren@gmail.com>
2025-02-06 22:11:13 +0100

    Finalized the server error page

--- client/src/app/errors/server-error/server-error.component.ts
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
@@ -6,4 +7,12 @@ import { Component } from '@angular/core';
   templateUrl: './server-error.component.html',
   styleUrl: './server-error.component.css',
 })
-export class ServerErrorComponent {}
+export class ServerErrorComponent {
+  error: any;
+
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation?.extras?.state?.['error'];
+
+  }
+}
