5e3b6e65e9dc29cddf22ae73e83a980bd7e57ed0
syntax-punk <david.jaeren@gmail.com>
2025-03-14 08:11:39 +0100

    Introducing a repository pattern

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,33 @@
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class UserRepository(DataContext context) : IUserRepository
+{
+    public void Update(AppUser user)
+    {
+        context.Entry(user).State = EntityState.Modified;
+    }
+
+    public async Task<bool> SaveAllAsync()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<IEnumerable<AppUser>> GetUsersAsync()
+    {
+        return await context.Users.ToListAsync();
+    }
+
+    public async Task<AppUser?> GetUserByIdAsync(int id)
+    {
+        return await context.Users.FindAsync(id);
+    }
+
+    public async Task<AppUser?> GetUserByUsernameAsync(string username)
+    {
+        return await context.Users.SingleOrDefaultAsync(u => u.UserName == username);
+    }
+}
