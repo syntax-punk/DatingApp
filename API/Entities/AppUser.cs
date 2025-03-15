b64812a497e457648c4dba32ad381e57a67d2a0c
syntax-punk <david.jaeren@gmail.com>
2025-03-15 13:22:09 +0100

    Added response dto to avoid cyclical reference problem between AppUser and Photos

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,7 +9,7 @@ public class AppUser
     public byte[] PasswordHash { get; set; } = [];
     public byte[] PasswordSalt { get; set; } = [];
     public DateOnly DateOfBirth { get; set; }
-    public string KnownAs { get; set; }
+    public required string KnownAs { get; set; }
     public DateTime Created { get; set; } = DateTime.UtcNow;
     public DateTime LastActive { get; set; } = DateTime.UtcNow;
     public required string Gender { get; set; }
@@ -18,7 +18,7 @@ public class AppUser
     public string? LookingFor { get; set; }
     public required string City { get; set; }
     public required string Country { get; set; }
-    public List<Photo> Photos { get; set; }
+    public List<Photo> Photos { get; set; } = [];
 
     public int GetAge()
     {
