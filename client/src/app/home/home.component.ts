8fe9e09397151c3e692d749b197941b49d61cbac
syntax-punk <david.jaeren@gmail.com>
2025-03-16 15:36:20 +0100

    Updated home and member list components

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +7,8 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
-  httpClient = inject(HttpClient);
+export class HomeComponent {
   registerMode = false;
-  users: any;
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
@@ -23,13 +16,5 @@ export class HomeComponent implements OnInit {
 
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
-  }
-
-  getUsers() {
-    this.httpClient.get('https://localhost:5001/api/users').subscribe({
-      next: (respose) => (this.users = respose),
-      error: (error) => console.error(error),
-      complete: () => console.log('Request completed'),
-    });
   }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/Member';
 
 @Component({
   selector: 'app-member-list',
@@ -6,4 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css',
 })
-export class MemberListComponent {}
+export class MemberListComponent implements OnInit {
+  private memberService = inject(MembersService);
+
+  members: Member[] = [];
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: (members) => (this.members = members),
+    });
+  }
+}
