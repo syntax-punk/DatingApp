c1fd60cc2d0ddff025b76a34514205dc4a5dd20c
syntax-punk <david.jaeren@gmail.com>
2025-03-26 21:12:02 +0100

    Member edit wip

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Member } from '../../_models/Member';
+import { AccountService } from '../../_services/account.service';
+import { MembersService } from '../../_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  imports: [],
+  templateUrl: './member-edit.component.html',
+  styleUrl: './member-edit.component.css',
+})
+export class MemberEditComponent implements OnInit {
+  member?: Member;
+  private accountService = inject(AccountService);
+  private membersService = inject(MembersService);
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const user = this.accountService.currentUser();
+    if (!user) return;
+    this.membersService.getMember(user.username).subscribe({
+      next: (member) => (this.member = member),
+    });
+  }
+}
