2d28a39850dbbd6305f08555f7080340370aebe2
syntax-punk <david.jaeren@gmail.com>
2025-03-30 00:42:52 +0100

    Added a members signal to the memebers service

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/Member';
 import { AccountService } from './account.service';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -11,17 +12,29 @@ export class MembersService {
   private http = inject(HttpClient);
   private accountService = inject(AccountService);
   baseUrl = environment.apiUrl;
+  members = signal<Member[]>([]);
 
   getMember(username: string) {
+    const member = this.members().find((x) => x.username === username);
+    if (member !== undefined) return of(member);
+
     return this.http.get<Member>(`${this.baseUrl}users/${username}`);
   }
 
   getMembers() {
-    return this.http.get<Member[]>(`${this.baseUrl}users`);
+    return this.http.get<Member[]>(`${this.baseUrl}users`).subscribe({
+      next: (members) => this.members.set(members),
+    });
   }
 
   updateMember(member: Member) {
-    return this.http.put(`${this.baseUrl}users`, member);
+    return this.http.put(`${this.baseUrl}users`, member).pipe(
+      tap(() => {
+        this.members.update((members) =>
+          members.map((m) => (m.username === member.username ? member : m))
+        );
+      })
+    );
   }
 
   // deprecated: can be removed
